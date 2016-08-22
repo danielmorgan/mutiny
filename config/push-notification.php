d060e81db363420692572cd0d9cdc97b350bc9bd
Daniel Morgan <me@danielmorgan.co.uk>
2016-08-22 11:55:02 +0100

    Initial commit

---
+++ config/push-notification.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'test' => array(
+        'environment' => 'development',
+        'apiKey'      => 'AIzaSyDIx9NFDKdSR6UnE75bPyvaS4KhxnuFtM0',
+        'service'     => 'gcm'
+    )
+);
