84b1fb59e7c9122cd38704c30d1dc7008924cf82
Daniel Morgan <me@danielmorgan.co.uk>
2016-10-03 20:34:26 +0100

    Add navigation room description

--- app/Rooms/NavigationRoom.php
+++ app/Rooms/NavigationRoom.php
@@ -7,6 +7,6 @@ class NavigationRoom extends Room
     protected static $singleTableType = 'nav';
     protected static $throwInvalidAttributeExceptions = true;
     public $name = 'Navigation Room';
-    public $description = '@todo: Write description for this room.';
+    public $description = 'The navigation room is divided by transparent partitions covered in black lines describing complex orbits. An analogue radar display blinks gently in one corner. A black and white terminal sits at the center of the room listing discovered local targets in a simple text based interface.';
     public $image = '/img/locations/navigationroom.jpg';
 }
